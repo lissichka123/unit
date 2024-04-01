00eb4bcd611891e1177f04f65a6f6d94f5a073ad
Ekaterina Turkina <katrina.turkina@gmail.com>
2024-04-02 01:07:53 +0300

    branch testng

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+    public void testRemain(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWithCashback(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+}
